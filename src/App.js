b1bd30893be6ad3d68b2c0994b64e69c56d7d6f0
Đinh Hoàng Vũ <vudh.22itb@vku.udn.vn>
2024-07-25 10:51:59 +0700

    callApi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { routes } from './routes';
 import DefaultComponent from './components/DefaultComponent/DefaultComponent';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
 
 function App() {
+    // useEffect(() => {
+    //     fetchApi();
+    // }, []);
+    const fetchApi = async () => {
+        const res = await axios.get(
+            `${process.env.REACT_APP_API_URL}/product/getAll`
+        );
+        return res.data;
+    };
+    // Queries
+    const query = useQuery({ queryKey: ['todos'], queryFn: fetchApi });
+    console.log('query', query);
     return (
         <div>
             <Router>
